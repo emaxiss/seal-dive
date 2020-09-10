bdc0528336419eeff3daedb5f93d9258c93afec4
Max Evstafev <61100478+emaxiss@users.noreply.github.com>
2020-09-10 10:18:57 -0400

    bird pos and grav doing

--- js/game.js
+++ js/game.js
@@ -13,8 +13,23 @@ fg.src = "img/flappy_bird_fg.png";
 pipeUp.src = "img/flappy_bird_pipeUp.png";
 pipeBottom.src = "img/flappy_bird_pipeBottom.png";
 
+const gap = 90;
+const xPos = 10;
+let yPos = 150;
+const grav = 1;
+
 function draw () {
     ctx.drawImage(bg, 0, 0);
+
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+    requestAnimationFrame(draw);
 }
 pipeBottom.onload = draw;
 
