b1d09e5712bb3024d4cd21b8cd6cf8fbf3a494d5
Max Evstafev <61100478+emaxiss@users.noreply.github.com>
2020-09-10 09:57:02 -0400

    images,background

--- js/game.js
+++ js/game.js
@@ -1,1 +1,21 @@
-alert("Hello This is Flappy Bird Like game")
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
+
+const bird = new Image();
+const bg = new Image();
+const fg = new Image();
+const pipeUp = new Image();
+const pipeBottom = new Image();
+
+bird.src = "img/flappy_bird_bird.png";
+bg.src = "img/flappy_bird_bg.png";
+fg.src = "img/flappy_bird_fg.png";
+pipeUp.src = "img/flappy_bird_pipeUp.png";
+pipeBottom.src = "img/flappy_bird_pipeBottom.png";
+
+function draw () {
+    ctx.drawImage(bg, 0, 0);
+}
+pipeBottom.onload = draw;
+
+
