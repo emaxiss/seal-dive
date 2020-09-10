6b1cfa5179e1fdfdf7591a52dade4a88ad1455e9
Max Evstafev <61100478+emaxiss@users.noreply.github.com>
2020-09-10 14:08:25 -0400

    running block added

--- js/game.js
+++ js/game.js
@@ -13,16 +13,25 @@ fg.src = "img/flappy_bird_fg.png";
 pipeUp.src = "img/flappy_bird_pipeUp.png";
 pipeBottom.src = "img/flappy_bird_pipeBottom.png";
 
+const pipe = [];
+pipe[0] = {
+    x : cvs.width,
+    y : 0
+}
+
 const gap = 90;
 const xPos = 10;
 let yPos = 150;
 const grav = 1;
 
-function draw () {
+function draw() {
     ctx.drawImage(bg, 0, 0);
+    for (let i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+    pipe[i].x--;
+    }
 
-    ctx.drawImage(pipeUp, 100, 0);
-    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
 
     ctx.drawImage(fg, 0, cvs.height - fg.height);
 
@@ -31,6 +40,11 @@ function draw () {
     yPos += grav;
     requestAnimationFrame(draw);
 }
+
 pipeBottom.onload = draw;
 
+document.addEventListener("keydown", moveUp);
 
+function moveUp() {
+    yPos -= 30;
+}
